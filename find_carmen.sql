ccbf0431efc18e79f129bddf1c9f3fbb97260f24
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-11 17:51:41 -0400

    answered #6

--- find_carmen.sql
+++ find_carmen.sql
@@ -62,8 +62,15 @@ WHERE code = 'BRA'
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+SELECT * FROM city
+JOIN country
+ON city.countrycode = country.code
+WHERE country.code = 'BRA'
+AND city.id = country.capital
 
-
+--or this 
+SELECT * FROM city
+WHERE id = 211
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
