ef649ce8049e4e13dfa264aec5782f9a600d64c2
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-11 11:25:16 -0400

    adjust answers to #2 & #3

--- find_carmen.sql
+++ find_carmen.sql
@@ -31,26 +31,28 @@ LIMIT 1
  
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-SELECT countrylanguage.language FROM countrylanguage
-JOIN country
-ON countrylanguage.countrycode = country.code
-WHERE country.region = 'Southern Europe'
-ORDER BY country.population ASC 
-LIMIT 1
+SELECT * FROM countrylanguage
+WHERE countrycode = 'VAT'
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 SELECT * FROM country
 JOIN countrylanguage
-ON countrylanguage.countrycode = country.code 
+ON country.code = countrylanguage.countrycode
 WHERE countrylanguage.language = 'Italian'
-AND country.region = 'Southern Europe'
-
+ORDER BY percentage DESC
+LIMIT 1
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-
-
+-- SELECT * FROM city
+-- JOIN country
+-- ON city.countrycode = country.code
+-- INNER JOIN countrylanguage
+-- ON country.code = countrylanguage.countrycode 
+-- WHERE countrylanguage.language = 'Italian'
+-- AND country.region = 'Southern Europe'
+-- AND city.name != country.name
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
